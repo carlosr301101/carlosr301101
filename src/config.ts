72dbf805af20daf266cb49e3f5e9e1b6b03a6f2c
carlosr301101 <carlosr301101@gmail.com>
2025-11-14 23:59:39 -0500

    fix: SEO changed h1 for h2

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Int-64 | Personal Portfolio and Blog'
+export const SITE_TITLE = 'Personal Portfolio and Blog'
 export const SITE_DESCRIPTION =
 	'This is a space for science, algorithms and much more. Website with Blog, CV, Project Section, Store and RSS Feed.'
 export const GENERATE_SLUG_FROM_TITLE = true
